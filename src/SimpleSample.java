52de3060572ad32c395331df13224d11e00a8e68
berak <px1704@web.de>
2017-12-24 13:25:00 +0100

    java face 6

--- src/SimpleSample.java
+++ src/SimpleSample.java
@@ -8,11 +8,25 @@ import org.opencv.imgproc.*;
 class SimpleSample {
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
+    public static void main(String[] args) {
         Mat ocv = new Mat(300,300,0);
         FaceRecognizer fr = Face.createLBPHFaceRecognizer();
 		System.out.println("fr: " + fr);
-		FacemarkKazemi kaz = FacemarkKazemi.create();
-		System.out.println("kz: " + kz);
+
+		FacemarkKazemi kaz;
+        try {
+			FacemarkKazemi kaz = FacemarkKazemi.create();
+			System.out.println("kz: " + kz);
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+        try {
+			kaz.loadModel("no.dat");
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
         System.exit(0); // to break out of the ant shell.
     }
 }
