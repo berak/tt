b5c3ffcc07542a2fb518c5b2f9bf30a9fed52b77
berak <px1704@web.de>
2017-12-24 13:38:20 +0100

    java face 7

--- src/SimpleSample.java
+++ src/SimpleSample.java
@@ -10,23 +10,33 @@ class SimpleSample {
 
     public static void main(String[] args) {
         Mat ocv = new Mat(300,300,0);
-        FaceRecognizer fr = Face.createLBPHFaceRecognizer();
+        FaceRecognizer fr = LBPHFaceRecognizer.create();
 		System.out.println("fr: " + fr);
 
+		System.out.println("kz: 1");
 		FacemarkKazemi kaz;
         try {
 			FacemarkKazemi kaz = FacemarkKazemi.create();
-			System.out.println("kz: " + kz);
+			System.out.println("kz: " + kaz);
         } catch (Exception e) {
             System.out.println(e);
-            return;
         }
+		System.out.println("kz: 2");
         try {
 			kaz.loadModel("no.dat");
         } catch (Exception e) {
             System.out.println(e);
-            return;
         }
+
+		System.out.println("kz: 3");
+        try {
+	        List<MatOfPoint2f> contours = new ArrayList<MatOfPoint2f>();
+	        List<MatOfRect> rects = new ArrayList<MatOfRect>();
+			kaz.fit(ocv,rects,contours);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
         System.exit(0); // to break out of the ant shell.
     }
 }
