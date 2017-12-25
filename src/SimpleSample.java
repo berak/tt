de9887b2fd6ed6b61e9db46026c4cf696d4a884b
berak <px1704@web.de>
2017-12-25 10:44:38 +0100

    java face 30

--- src/SimpleSample.java
+++ src/SimpleSample.java
@@ -39,7 +39,7 @@ class SimpleSample {
 
 		System.out.println("kz: 3");
         try {
-	        java.util.List<Mat> contours = new ArrayList<Mat>();
+	        java.util.List<Mat> contours = new ArrayList<Mat>(1);
 	        //Mat contours = new Mat();
 	        MatOfRect rects = new MatOfRect(new Rect(20,20,100,100));
 			//kaz.fit(ocv,rects,contours);
