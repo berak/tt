95b27a7b19cf784c359edf39cd4d7f913431f8a1
berak <px1704@web.de>
2017-12-31 12:57:15 +0100

    mace #1

--- src/SimpleSample.java
+++ src/SimpleSample.java
@@ -4,8 +4,7 @@ import org.opencv.core.*;
 import org.opencv.imgproc.*;
 
 
-import org.opencv.bgsegm.*;
-import org.opencv.bgsegm.Bgsegm.*;
+import org.opencv.face.*;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
@@ -17,37 +16,16 @@ class SimpleSample {
     public static void help(String cls){ ClassSpy.reflect(cls,"CLASS"); }
     public static void help(String cls,String item){ ClassSpy.reflect(cls,item); }
 
-    public static void testCNT() {
-        System.err.println("!! running CNT test ! ");
-        BackgroundSubtractorCNT bgs = Bgsegm.createBackgroundSubtractorCNT();
-        System.err.println("CNT inst " + bgs);
-	    Mat img = new Mat(300,300,CvType.CV_8U);
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        System.err.println("CNT mask " + mask);
-    }
-    public static void testMOG() {
-        System.err.println("!! running MOG test ! ");
-        BackgroundSubtractorMOG bgs = Bgsegm.createBackgroundSubtractorMOG();
-        System.err.println("MOG inst " + bgs);
-	    Mat img = new Mat(300,300,CvType.CV_8U);
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        System.err.println("MOG mask " + mask);
-    }
-    public static void testGSOC() {
-        System.err.println("!! running GSOC test ! ");
-        BackgroundSubtractorGSOC bgs = Bgsegm.createBackgroundSubtractorGSOC();
-        System.err.println("GSOC inst " + bgs);
-	    Mat img = new Mat(300,300,CvType.CV_8U);
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        System.err.println("GSOC mask " + mask);
-    }
     public static void main(String[] args) {
-    	testCNT();
-    	testMOG();
-    	testGSOC();
+        MACE mace = Face.MACE.create();
+        mace.salt("12325454");
+        ArrayList<Mat> imgs = new ArrayList<Mat>();
+        for (int i=0; i<20; i++) {
+            imgs.add(new Mat(64,64,CV_8U));
+        }
+        mace.train(imgs);
+        Mat m = new Mat(64,64,CV_8U);
+        bool s = mace.same(m);
         System.exit(0); // to break out of the ant shell.
     }
 }
