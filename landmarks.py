76836b911e057c65c3680cde962bb1a0da101fb5
berak <px1704@web.de>
2018-06-14 14:59:13 +0200

    face python 9

--- landmarks.py
+++ landmarks.py
@@ -1,3 +1,8 @@
+import sys
+print(sys.path)
+sys.path.append('/home/travis/build/berak/tt/ocv/lib/python2.7/dist-packages/')
+print(sys.path)
+
 import cv2, numpy as np
 
 img = cv2.imread("david2.jpg",0)
@@ -6,4 +11,4 @@ obj.loadModel("lbfmodel.yaml")
 cas = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 faces = face_cascade.detectMultiScale(img, 1.5, 5)
 ok, landmarks = obj.fit(img, faces)
-print ok, landmarks
+print (ok, landmarks)
