33d6bc8bd9b0caea3ce48c545a3bd0aa7d69d232
berak <px1704@web.de>
2018-06-15 09:11:13 +0200

    face 19

--- src/SimpleSample.java
+++ src/SimpleSample.java
@@ -1,44 +1,50 @@
-package org.opencv.test.bgsegm;
-
-import org.opencv.bgsegm.*;
-import org.opencv.bgsegm.Bgsegm.*;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
+import org.opencv.face.*;
+import org.opencv.imgcodecs.*;
+import org.opencv.imgproc.*;
+import org.opencv.objdetect.*;
+import java.util.*;
 
 
 public class SimpleSample {
-    static Mat img = new Mat(300,300,CvType.CV_8U);
-
-
-    public static void testCNT() throws Exception {
-        BackgroundSubtractorCNT bgs = Bgsegm.createBackgroundSubtractorCNT();
-        if(bgs == null) throw new Exception("could not create a CNT instance!");
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        if (mask.empty()) throw new Exception("no mask created from CNT");
-    }
-    public static void testMOG() throws Exception {
-        BackgroundSubtractorMOG bgs = Bgsegm.createBackgroundSubtractorMOG();
-        if(bgs == null) throw new Exception("could not create a MOG instance!");
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        if (mask.empty()) throw new Exception("no mask created from MOG");
-    }
-    public static void testGSOC() throws Exception {
-        BackgroundSubtractorGSOC bgs = Bgsegm.createBackgroundSubtractorGSOC();
-        if(bgs == null) throw new Exception("could not create a GSOC instance!");
-        Mat mask = new Mat();
-        bgs.apply(img,mask);
-        if (mask.empty()) throw new Exception("no mask created from GSOC");
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
     public static void main(String[] args) {
-        try {
-            testCNT();
-            testMOG();
-            testGSOC();
-        } catch (Exception e) {System.out.println(e);}
+        /*if (args.length < 3) {
+            System.out.println("use: java Facemark [image file] [cascade file] [model file]");
+            return;
+        }*/
+        // read the image
+        Mat img = Imgcodecs.imread("david2.jpg");
+
+        // setup face detection
+        CascadeClassifier cascade = new CascadeClassifier("haarcascade_frontalface_alt2.xml");
+        MatOfRect faces = new MatOfRect();
+        // detect faces
+        cascade.detectMultiScale(img, faces);
+
+        // setup landmarks detector
+        // Facemark fm = Face.createFacemarkKazemi();
+       // fm.loadModel("C:\\data\\mdl\\face_landmark_model.dat");
+        Facemark fm = Face.createFacemarkLBF();
+        fm.loadModel("lbfmodel.yaml");
+
+        // fit landmarks for each found face
+        ArrayList<MatOfPoint2f> landmarks = new ArrayList<MatOfPoint2f>();
+        fm.fit(img, faces, landmarks);
+
+        // draw them
+        for (int i=0; i<landmarks.size(); i++) {
+            MatOfPoint2f lm = landmarks.get(i);
+            for (int j=0; j<lm.rows(); j++) {
+                double [] dp = lm.get(j,0);
+                Point p = new Point(dp[0], dp[1]);
+                Imgproc.circle(img,p,2,new Scalar(222),1);
+            }
+        }
+        // save result
+        Imgcodecs.imwrite("landmarks.jpg",img);
     }
 }
