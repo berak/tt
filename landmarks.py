736d2c82414d33f8fd81a3f2787218dd69ce4f97
berak <px1704@web.de>
2018-06-16 10:58:51 +0200

    face 24

--- landmarks.py
+++ landmarks.py
@@ -1,15 +1,27 @@
 import sys
-print(sys.path)
 sys.path.append('/home/travis/build/berak/tt/ocv/lib/python2.7/dist-packages/')
-print(sys.path)
 
 import cv2, numpy as np
 
 img = cv2.imread("david2.jpg",0)
-obj = cv2.face.createFacemarkLBF()
-obj.loadModel("lbfmodel.yaml")
 cas = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 faces = cas.detectMultiScale(img, 1.5, 5)
 print("faces",faces)
+
+print("testing LBF")
+obj = cv2.face.createFacemarkLBF()
+obj.loadModel("lbfmodel.yaml")
 ok, landmarks = obj.fit(img, faces)
-print ("landmarks",ok, landmarks)
+print ("landmarks LBF",ok, landmarks)
+
+print("testing AAM")
+obj = cv2.face.createFacemarkAAM()
+obj.loadModel("aam.yaml")
+ok, landmarks = obj.fit(img, faces)
+print ("landmarks AAM",ok, landmarks)
+
+print("testing Kazemi")
+obj = cv2.face.createFacemarkKazemi()
+obj.loadModel("opencv/build/testdata/cv/face/face_landmark_model.dat")
+ok, landmarks = obj.fit(img, faces)
+print ("landmarks Kazemi",ok, landmarks)
