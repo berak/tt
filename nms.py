e8a3b36d5be8a1c3e1988ff3b3b4b1e307e82d1d
berak <px1704@web.de>
2018-08-24 08:45:21 +0200

    nms 1

---
+++ nms.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/home/travis/build/berak/tt/ocv/lib/python2.7/dist-packages/')
+
+import cv2, numpy as np
+
+boxes = np.array([[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]],np.float32)
+conf = np.array([.6, .5, .2], np.float32)
+indices = NMSBoxesRotated(boxes, conf, .1, .1);
+print("NMS", indices)
