2b67a7aa4f98717eb7a13a38542e9dec9bf446ae
berak <px1704@web.de>
2018-08-24 17:07:58 +0200

    nms 19

--- nms.py
+++ nms.py
@@ -6,9 +6,10 @@ import cv2, numpy as np
 #help(cv2.dnn.NMSBoxes)
 #help(cv2.dnn.NMSBoxesRotated)
 
-#boxes = np.array([[[1,2,3,4],[2,1,3,4],[4,3,2,1]]],np.int32)
-boxes = [[1,2,3,4],[2,1,3,4],[4,3,2,1]]
+boxes = np.array([[[1,2,3,4],[2,1,3,4],[4,3,2,1]]],np.int32)
+#boxes = [[1,2,3,4],[2,1,3,4],[4,3,2,1]]
 conf = [.6, .5, .2]
+#conf = np.array([.6, .5, .2],np.float32)
 #try:
 indices = cv2.dnn.NMSBoxes(boxes, conf, .1, .1)
 print("NMS", indices)
@@ -17,6 +18,7 @@ print("NMS", indices)
 
 
 rboxes = np.array([[[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]]],np.float32)
+rboxes = [[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]]
 #rboxes = [(1,2,3,4,5),(2,1,3,4,6),(4,3,2,1,2)]
 conf = [.6, .5, .2]
 try:
