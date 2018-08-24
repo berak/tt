9a70864e412e7605cb29860ee098de283515ba1b
berak <px1704@web.de>
2018-08-24 08:46:18 +0200

    nms 2

--- nms.py
+++ nms.py
@@ -5,5 +5,5 @@ import cv2, numpy as np
 
 boxes = np.array([[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]],np.float32)
 conf = np.array([.6, .5, .2], np.float32)
-indices = NMSBoxesRotated(boxes, conf, .1, .1);
+indices = cv2.dnn.NMSBoxesRotated(boxes, conf, .1, .1);
 print("NMS", indices)
