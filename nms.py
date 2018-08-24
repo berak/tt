576cae0c9195247d514a9c2bfee7c975e6e6d57f
berak <px1704@web.de>
2018-08-24 10:16:20 +0200

    nms 9

--- nms.py
+++ nms.py
@@ -3,7 +3,22 @@ sys.path.append('/home/travis/build/berak/tt/ocv/lib/python2.7/dist-packages/')
 
 import cv2, numpy as np
 
-boxes = np.array([[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]],np.float32)
+help(cv2.dnn)
+
+boxes = np.array([[[1,2,3,4],[2,1,3,4],[4,3,2,1]]],np.float32)
 conf = np.array([.6, .5, .2], np.float32)
-indices = cv2.dnn.NMSBoxesRotated(boxes, conf, .1, .1);
-print("NMS", indices)
+try:
+    indices = cv2.dnn.NMSBoxes(boxes, conf, .1, .1)
+    print("NMS", indices)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+
+
+rboxes = np.array([[[1,2,3,4,5],[2,1,3,4,6],[4,3,2,1,2]]],np.float32)
+conf = np.array([.6, .5, .2], np.float32)
+try:
+    indices = cv2.dnn.NMSBoxesRotated(rboxes, conf, .1, .1)
+    print("NMSRot", indices)
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+
